42192b117397a4a2d4a19e7afcd9ad0ed3238291
akrawczyk <agammkrawczyk@gmail.com>
2020-03-30 23:50:38 +0200

    #fix paht to img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {HomesComponent} from './homes/homes.component';
 import {AppRoutingModule} from './app-routing.module';
@@ -13,7 +12,8 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, HttpClientModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
