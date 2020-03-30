74a8cb7631315c1da4758e42ac28ec742264be73
akrawczyk <agammkrawczyk@gmail.com>
2020-03-30 22:10:21 +0200

    #clean app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomesComponent } from './homes/homes.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HomesComponent } from './homes/homes.component';
     HomesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
