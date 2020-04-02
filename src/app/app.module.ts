f0ea9af2ac737ffcbcdccb71355d825faf573ceb
akrawczyk <agammkrawczyk@gmail.com>
2020-04-02 16:02:28 +0200

    #angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import {AppComponent} from './app.component';
 import {HomesComponent} from './homes/homes.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
-import { HomeTypeFilterComponent } from './home-type-filter/home-type-filter.component';
+import {HomeTypeFilterComponent} from './home-type-filter/home-type-filter.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import { HomeSearchComponent } from './home-search/home-search.component';
-import { HomeComponent } from './home/home.component';
+import {HomeSearchComponent} from './home-search/home-search.component';
+import {HomeComponent} from './home/home.component';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -18,10 +19,12 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    MatSliderModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
